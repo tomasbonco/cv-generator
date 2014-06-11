862589616286ffa33f9b09ddb04f35df4388ea5f
Honza <Honza@Honza-PC>
2014-06-11 22:05:32 +0200

    PDFfromLatexBuilder Test added

---
+++ java/CV-Generator/test/cz/muni/fi/PDFfromLatexBuilderTests.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi;
+
+import cz.muni.fi.classes.PDFfromLatexBuilder;
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jan Polišenský <polisensky88 at gmail.com>
+ */
+public class PDFfromLatexBuilderTests {
+    
+    private PDFfromLatexBuilder pflb;
+    private File latexFile;
+    private File pdfFile;
+    
+    public PDFfromLatexBuilderTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        pflb = new PDFfromLatexBuilder("C:\\Users\\Honza\\Disk Google\\Tex\\test");
+        latexFile = new File("test_files","simple_latex_cv");
+        pdfFile = new File(pflb.createPDF(latexFile));
+    }
+    
+    @After
+    public void tearDown() {
+        pdfFile.delete();
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void PDFCreated(){
+        assertTrue(pdfFile.exists());
+    }
+    
+    @Test
+    public void intermediateDviDeleted(){
+        assertFalse(new File(latexFile.getAbsolutePath()+".dvi").exists());
+    }
+    
+    @Test
+    public void intermediateLogDeleted(){
+        assertFalse(new File(latexFile.getAbsolutePath()+".log").exists());
+    }
+    
+    @Test
+    public void intermediateOutDeleted(){
+        assertFalse(new File(latexFile.getAbsolutePath()+".out").exists());
+    }
+    
+    @Test
+    public void intermediateAuxDeleted(){
+        assertFalse(new File(latexFile.getAbsolutePath()+".aux").exists());
+    }
+    
+}
